981bf2b28b98fb1a4098c1987090dcaa07d03ef1
Vinícius Hoyer <vhoyer@live.com>
2018-11-10 20:54:11 +0000

    add markdown parser

--- pages/article/_id/index.vue
+++ pages/article/_id/index.vue
@@ -3,14 +3,13 @@
     <small>{{ path }}</small>
     <h2>{{ title }}</h2>
 
-    <div>
-      {{ body }}
-    </div>
+    <div v-html="marked"/>
   </article>
 </template>
 
 <script>
 import { firestore } from '~/plugins/firebase'
+import marked from 'marked'
 
 export default {
   async asyncData({ params, error }) {
@@ -27,6 +26,18 @@ export default {
       title: snap.title,
       body: snap.body,
     }
+  },
+  computed: {
+    marked() {
+      const { lexer, parser } = marked
+
+      return parser(lexer(this.body))
+    }
+  },
+  created() {
+    marked.setOptions({
+      sanitize: true,
+    })
   },
 }
 </script>
