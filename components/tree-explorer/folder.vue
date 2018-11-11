751607247c3e02fa41977c2f3d27697f58fce76e
Vinícius Hoyer <vhoyer@live.com>
2018-11-11 01:14:31 +0000

    add code to push tree to firestore

--- components/tree-explorer/folder.vue
+++ components/tree-explorer/folder.vue
@@ -5,7 +5,7 @@
     >
       {{ folderName }}
     </summary>
-    <button>+ add node</button>
+    <button @click="addNode">+ add node</button>
 
     <folder-content
       :articles="articles"
@@ -47,15 +47,35 @@ export default {
   },
   methods: {
     ...mapActions('tree-view', {
+      addFolderAndArticle: 'addFolderAndArticle',
       getNode: 'getNode',
     }),
     openFolder() {
       this.getNode(this.path)
-      .then(({ articles, folders }) => {
-        this.articles = articles
-        this.folders = folders
-      })
+        .then(({ articles, folders }) => {
+          this.articles = articles
+          this.folders = folders
+        })
     },
+    addNode() {
+      let input = prompt("New file: (To create a folder append a '/' to the end)")
+      let folder = this.path
+
+      while (input.includes('=+')) {
+        alert("Feeling smarty-pants, ye?\nYou cannot use '=+' because of reasons. Try again.")
+
+        input = prompt("New file: (To create a folder append a '/' to the end)", input)
+      }
+
+      if (input.includes('/')) {
+        let path = input.split('/')
+        input = path.pop()
+
+        folder += path.reduce((acc,cur) => `${acc}=+${cur}`)
+      }
+
+      this.addFolderAndArticle({path: folder, name: input});
+    }
   },
 }
 </script>
--- store/tree-view.js
+++ store/tree-view.js
@@ -1,4 +1,5 @@
 import { firestore } from '~/plugins/firebase'
+const treeView = firestore.collection('tree-view')
 
 export const state = () => ({
 })
@@ -10,12 +11,12 @@ export const mutations = {
 }
 
 export const actions = {
-  async getNode({commit, state, dispatch}, path) {
+  async getNode({commit, state}, path) {
     if (state.hasOwnProperty(path)) {
       return state[path]
     }
     //getFromFirebase
-    const doc = await firestore.collection('tree-view').doc(path).get()
+    const doc = await treeView.doc(path).get()
     commit('pushNode', {
       [doc.id]: {
         folders: [],
@@ -25,5 +26,36 @@ export const actions = {
     })
 
     return state[path]
-  }
+  },
+  //Sorry sometimes I can't help but to write shitty code, I find it funny
+  async addFolderAndArticle({commit, state}, {path, name}) {
+    const articleId = name ? firestore.collection('articles').add({body: '', path: path, title: name}).then(docRef => docRef.id) : null
+
+    let tail = ''
+    const structure = path.split('=+').filter(n => n).map((val) => {
+      return {
+        parent: tail ? tail : "=+",
+        folder: tail += '=+' + val,
+      }
+    }).concat({parent: path})
+
+    const tryAddFolder = async ({parent, folder}) => {
+      const doc = await treeView.doc(parent).get()
+
+      const node = { folders: [], articles: {}, ...(doc.exists ? doc.data() : {}) }
+      if (folder) {
+        node.folders.push(folder)
+        node.folders = Array.from(new Set(node.folders))
+      }
+      if (path === parent && name) {
+        node.articles[await articleId] = name
+      }
+
+      commit('pushNode', { [parent]: node })
+      return treeView.doc(parent).set(node)
+    }
+    const promises = structure.map((element) => tryAddFolder(element))
+
+    return Promise.all(promises)
+  },
 }
